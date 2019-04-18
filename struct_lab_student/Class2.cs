5f1bfda70c7f45d5849303a7eeeb5fbceab96f43
maxutor <maxutor@ukr.net>
2019-04-18 20:13:53 +0300

    Initial commit

---
+++ struct_lab_student/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    partial class Maksym
+    {
+        public void Variant15(Student[] studs)
+        {
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].physicsMark > '2' && studs[i].mathematicsMark > '2' && studs[i].informaticsMark > '2')
+                {
+                    string s = $"{studs[i].surName} {studs[i].firstName} {studs[i].patronymic} {studs[i].scholarship}";
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
