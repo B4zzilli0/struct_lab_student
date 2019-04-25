c3a85270e49fc01628a386e80826a98e76013623
B4zzilli0 <bazzillio.in.br000klyn@gmail.com>
2019-04-25 23:44:04 +0300

    ))))

---
+++ struct_lab_student/Class3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    partial class Jenya
+    {
+        public void Variant13(Student[] studs)
+        {
+            Console.WriteLine("Cтуденти, що пропустили бiльше двох iспитiв:");
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].physicsMark == '-' && studs[i].mathematicsMark == '-' && studs[i].informaticsMark == '-')
+                {
+                    string s = $"{studs[i].surName}";
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
